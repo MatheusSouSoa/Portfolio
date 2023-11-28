315bee0145f1998a8751d19bae15804aa9a28e90
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-11-28 11:05:33 -0300

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -109,7 +109,7 @@ const Work = () => {
               <p className="mb-6">
                       De agosto a novembro, fui responsável pela execução de um projeto fullstack de validação de templates, atuando de forma independente. No backend, desenvolvi a lógica em Node.js e Flask, utilizando Flask com Pandas para validar colunas Excel e assegurar a integridade dos dados. Implementei a autenticação de usuários usando JWT no Node.js, e empreguei tecnologias como TypeORM, Axios, Jest e ESLint.
   
-  Posteriormente, realizei uma transição para a área de Data Analytics, onde estou atuando em análise de dados, interpretação de resultados e geração de insights estratégicos. 
+  Desde dezembro de 2023 estou alocado no setor de data analytics.
               </p>
             </motion.div>
 
